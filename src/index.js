341ff72feefceb00ef8d12701719863c560d909a
Hoang <nguyenhuyhoang112k@gmail.com>
2022-09-28 00:50:59 +0700

    config layout

--- src/index.js
+++ src/index.js
@@ -9,13 +9,19 @@ const port = 3000
 //HTTP logger
 app.use(morgan('combined'))
 
+// Template engine
+
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, '/resources/views'));
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
 })
 
 app.listen(port, () => {
