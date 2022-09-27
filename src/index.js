6f13bd2c0f9fe0f0bc391cd7a4f9493075b32a3d
Nguyen_hoang <nguyenhuyhoang112k@gmail.com>
2022-09-27 17:58:47 +0700

    set up

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+  // const handlebars =require('express-handlebars') ;
+  const { engine } = require("express-handlebars");
+  const path = require('path')
+const app = express()
+const port = 3000
+
+//HTTP logger
+app.use(morgan('combined'))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
