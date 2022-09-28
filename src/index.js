b4df9f2391879ea72886ecccc4e5f41197a54eef
Nguyen_hoang <nguyenhuyhoang112k@gmail.com>
2022-09-28 13:57:11 +0700

    scss

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname,'public')));
 //HTTP logger
 app.use(morgan('combined'))
 
 // Template engine
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({extname:'.hbs'}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 
 
